0d0c69eb27d6ee9ab0435ce59257f8b18c47a6b6
EsromCarlos <esromcarlosg@gmail.com>
2018-09-05 11:30:27 -0300

    criação da barra de navegação

---
+++ index.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.bundle.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+            <a class="nav-link" href="#">Página Inicial <span class="sr-only">(Ativo)</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Gerenciar Questões</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Gerenciar Provas</a>
+        </li>
+        <!-- DropDown
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dropdown
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+        </li>
+        -->
+        </ul>
+    </div>
+    </nav>
+</body>
+</html>
